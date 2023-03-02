98bd776b695c6c53baf56ecf5d79597f5de45bad
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-02 13:28:11 -0600

    completed partition function that will be called in quick sort function

--- src/quickSort.js
+++ src/quickSort.js
@@ -4,7 +4,39 @@ function quickSort(
   lowerIndex = 0,
   upperIndex = elements.length - 1
 ) {
-  return elements;
+  // pivot
+  // partitionIndex
+}
+
+// this function returns???
+function partition(compare, elements, lowerIndex, upperIndex) {
+  // pivotValue is an upperIndex, left
+  // elements.length-1
+  const pivotValue = elements[upperIndex];
+  // partiionIndex is a lowerIndex
+  // 0
+  let partitionIndex = lowerIndex;
+
+  for (let index = lowerIndex; index < upperIndex; index++) {
+    const comparison = compare(pivotValue, elements[index]);
+
+    if (comparison > 0) {
+      if (partitionIndex !== index) {
+        // switch the elements
+        [elements[index], elements[partitionIndex]] = [
+          elements[partitionIndex],
+          elements[index],
+        ];
+      }
+      partitionIndex++;
+    }
+  }
+  //
+  [elements[partitionIndex], elements[upperIndex]] = [
+    elements[upperIndex],
+    elements[partitionIndex],
+  ];
+  return partitionIndex;
 }
 
 module.exports = quickSort;
